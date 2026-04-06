9542a7033ed4a621b27c13f7f9c8712024f49f5d
Bantuan adakami <144202158+cs-adakami@users.noreply.github.com>
2026-04-06 19:27:05 +0700

    Update contact.ts

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -1,5 +1,5 @@
-const DISPLAY_PHONE = '0818773020';
-const WA_NUMBER = '62818773020';
+const DISPLAY_PHONE = '08217506899';
+const WA_NUMBER = '628217506899';
 const WA_MESSAGE = 'Hallo CS Adakami, saya butuh bantuan';
 
 export const CONTACT = {
